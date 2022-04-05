8703690ad44154b42e0646603c37a01873666900
ZBreezie <zpmc55@yahoo.com>
2022-04-05 00:51:26 -0400

    about touch up

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,13 +9,13 @@ const About = () => {
   return (
     <div className="main-color container-fluid text-white">
       <div className=" row">
-        <h1 className=" display-1 col text-center">
+        <h1 className=" display-1 col">
           Hello there!:
         </h1>
         <div className='main-color row justify-content-around'>
             <p className="aboutp card bg-dark col-7 text-center">My name is Zachary, and I'm a Full-Stack developer! I grew up in Charlotte, NC, and I am currently based in Richmond, VA!
             </p>
-            <div className="aboutp col-4 text-center"> 
+            <div className="aboutp col-2 text-center"> 
               <a href={github} target="_blank" rel="noreferrer" className=" card main-color col-12">
                 <FontAwesomeIcon icon={faGithub} size="2x"/>
                 <p className="bg-dark text-white col-md-offset-2">Check out my GitHub!</p>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,7 +10,7 @@ const Header = (props) => {
   } = props;
 
   return (
-    <header className="d-flex align-items-center flex-wrap justify-content-center py-3 mb-4 border-bottom bg-dark text-white sticky-top">
+    <header className="d-flex align-items-center flex-wrap justify-content-center  mb-4 border-bottom bg-dark text-white sticky-top">
         <div style={{ marginLeft: "5%" }} className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none fs-1">Zachary McCollum's Portfolio</div>
       <Nav
         navSection={navSection}
